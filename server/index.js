60b78c6956b96909c63b37050d6c02c8e8bf9f70
kazuki <poohsan8302@gmail.com>
2025-10-09 16:33:23 +0900

    ver6.7.4

--- server/index.js
+++ server/index.js
@@ -37,13 +37,12 @@ const ALLOWLIST = [
 const corsDelegate = (req, cb) => {
   const origin = req.header("Origin") || "";
   const ok =
-    !origin ||
     ALLOWLIST.includes(origin) ||
     origin.includes("localhost") ||
     origin.includes("127.0.0.1");
   cb(null, {
-    origin: ok,
-    credentials: false,
+    origin: ok ? origin : false,
+    credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Timezone"],
     optionsSuccessStatus: 204,
